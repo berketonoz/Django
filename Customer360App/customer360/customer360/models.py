0ea058920e25e8829236b32179b139ead04fe02b
berketonoz <btonoz@sabanciuniv.edu>
2024-03-21 20:34:49 +0300

    Created views and added urlpatterns

--- Customer360App/customer360/customer360/models.py
+++ Customer360App/customer360/customer360/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib  import admin
 
 class Customer(models.Model):
     id = models.AutoField(primary_key=True)
@@ -6,6 +7,7 @@ class Customer(models.Model):
     email = models.EmailField(max_length=100)
     phone = models.CharField(max_length=20)
     address = models.CharField(max_length=200)
+    social_media = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return str(self.id)
@@ -16,6 +18,7 @@ class Interaction(models.Model):
         ('sms', 'SMS'),
         ('email', 'Email'),
         ('letter', 'Letter'),
+        ('social_media', 'Social Media'),
     ]
 
     DIRECTION_CHOICES = [
@@ -29,4 +32,5 @@ class Interaction(models.Model):
     interaction_date = models.DateField(auto_now_add=True)
     summary = models.TextField()
 
-    
+admin.site.register(Customer)
+admin.site.register(Interaction)
